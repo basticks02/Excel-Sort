50280030dfc2e58beccb7df8100c007a0da402b6
Ekomobong Ekanem <basticks@Ekomobongs-MacBook-Air.local>
2024-09-05 05:15:11 -0400

    Completed Script

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import pandas as pd
-df = pd.read_excel('calls_export_09-03-2024_3-53-15_pm.xlsx')
+df = pd.read_excel('calls_export_09-03-2024_3-53-15_pm.xlsx', header=1)
 
+def find_unanswered_phone_calls(df):
+    unanswered_calls = []
+    inbound_calls = df[df['Type'] == 'inbound']
+    grouped = inbound_calls.groupby('Calling Number')
 
-selected_columns = df['Date']
-print(selected_columns)
+    for calling_number, group in grouped:
+        if (group['Duration (min)'] == '0s').all():
+            unanswered_calls.append(calling_number)
+
+    return unanswered_calls
+
+unanswerd_in_calls = find_unanswered_phone_calls(df)
+print(unanswerd_in_calls)
+
+# print(df.columns)
+
 
